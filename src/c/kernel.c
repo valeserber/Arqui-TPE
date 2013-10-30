3dfc3a19d9653d3737185a51f697f0bb0453da30
contiver <cristianontivero@gmail.com>
2013-10-30 18:57:51 -0300

    pequeños cambios

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -1,8 +1,8 @@
 #include "../include/kasm.h"
 #include "../include/defs.h"
 
-DESCR_INT idt[0xA];			/* IDT de 10 entradas*/
-IDTR idtr;				/* IDTR */
+DESCR_INT idt[0xA];     /* IDT de 10 entradas*/
+IDTR idtr;              /* IDTR */
 
 int tickpos=640;
 
@@ -14,57 +14,52 @@ void int_08() {
 }
 
 /**********************************************
-kmain() 
-Punto de entrada de cÃ³o C.
+kmain()
+Punto de entrada de código C.
 *************************************************/
 
-kmain() 
+kmain()
 {
+    int i,num;
 
-        int i,num;
+/* Borra la pantalla. */
 
-/* Borra la pantalla. */ 
-
-	k_clear_screen();
+    k_clear_screen();
 
 /*Imprime hola mundo. */
 
-	print_hello();
+    print_hello();
 
-	/*
-	print("Hello");
-	char buffer[20];
-	long prueba=1234;
-	print(to_string(buffer,prueba));
-	*/
+    /*
+    print("Hello");
+    char buffer[20];
+    long prueba=1234;
+    print(to_string(buffer,prueba));
+    */
 /*Calcula cantidad de memoria*/
-	char buffer[20];
-	print(to_string(buffer,get_memory()));
+    char buffer[20];
+    print(to_string(buffer,get_memory()));
 
 
 /* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ0    */
 
         setup_IDT_entry (&idt[0x08], 0x08, (dword)&_int_08_hand, ACS_INT, 0);
-	
+
 /* Carga de IDTR    */
 
-	idtr.base = 0;  
-	idtr.base +=(dword) &idt;
-	idtr.limit = sizeof(idt)-1;
-	
-	_lidt (&idtr);	
+    idtr.base = 0;
+    idtr.base +=(dword) &idt;
+    idtr.limit = sizeof(idt)-1;
 
-	_Cli();
+    _lidt (&idtr);
+
+    _Cli();
 /* Habilito interrupcion de timer tick*/
 
-        _mascaraPIC1(0xFE);
-        _mascaraPIC2(0xFF);
-        
-	_Sti();
+    _mascaraPIC1(0xFE);
+    _mascaraPIC2(0xFF);
+    _Sti();
 
-        while(1)
-        {
-        }
-	
+    while(1){}
 }
 
