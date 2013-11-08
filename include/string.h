9d129c03161089d898e3a79cf328195a93adbdb9
contiver <user@arquitecturas.(none)>
2013-11-08 16:14:57 -0300

    scanf implementada

--- include/string.h
+++ include/string.h
@@ -6,5 +6,6 @@
 size_t strcmp(const char *s, const char *t);
 size_t strlen(const char * str);
 char* ltoa(long value, char* buffer, int radix);
+int atoi(const char * str);
 
 #endif
--- src/c/stdio.c
+++ src/c/stdio.c
@@ -2,6 +2,7 @@
 #include "../../include/stdarg.h"
 #include "../../include/stdio.h"
 #include "../../include/string.h"
+#include "../../include/ctype.h"
 
 int vfprintf(int fd, const char * fmt, va_list ap);
 void printFlags(dword flags);
@@ -122,106 +123,86 @@ int scanf(const char * fmt, ...){
     char *p, *sval;
 }
 
+*/
 
 int scanf(const char * fmt, ...){
-	va_list ap; //apunta a cada arg sin nombre en orden
-	char *p, *sval, *cval;
-	int *ival;
-	FILE  fp;
-	int read=0,i=0, c,k, j = 0;
-	va_start(ap, fmt); //hace que ap apunte al 1er arg sin nombre
-	char s[BUFFER_SIZE];
-	while((c = getc(&fp)) != '\n' && i < BUFFER_SIZE)
-	{
-		if(c == '\b' && i >= 1)
-			i--;
-		if(c != '\b')
-		{
-			s[i] = c;
-			i++;
-		}
-	}
-	s[i] = '\0';
-	i = 0;
-		for (p = fmt; *p; p++) 
-		{
-			char aux [BUFFER_SIZE] = {0};
+    va_list ap; //apunta a cada arg sin nombre en orden
+    char *p, *sval, *cval;
+    char s[50];
+    int *ival;
+    int charsRead=0,i=0, c,k, j;
+    va_start(ap, fmt); //hace que ap apunte al 1er arg sin nombre
 
-			if(*p != '%')
-			{
-				c = s[j++];
-				if(c =='\0' || *p != c)
-					return read;
-				continue;
-			}
-			switch (*++p)
-			{
-				case 'd':
-							i=0;
-							ival = va_arg(ap,int *);
-							while(s[j] != '\0' && isdigit(s[j]))
-							{
-								aux[i++] = s[j++];
-							}
-							aux[i] = '\0';
-							if(aux[0] == '\0')
-								return read;
-							*ival=atoi(aux);
-							read++;
-							break;
-				case 's':
-							i = 0;
-							sval = va_arg(ap,char *);
-							while(s[j] != '\0' && s[j] != ' ')
-							{
-								aux[i++] = s[j++];
-							}
-							aux[i] = '\0';
-							if(aux[0] == '\0')
-								return read;
-							
-							k = 0;
-							while(aux[k] != '\0')
-							{
-								sval[k] = aux[k];
-								k++;
-							}
-							sval[k] = '\0';
-							read++;
-							break;
-				case 'c':
-							cval = va_arg(ap,char*);
-							c = s[j++];
-							if(c == '\0' )
-								return read;
-							*cval = c;
-							read++;
-							break;
-				case 'x':
-							i=0;
-							ival = va_arg(ap,int *);
-							while(s[j] != '\0' && (isdigit(s[j]) || ishexa(s[j])))
-							{
-								aux[i] = s[j];
-								i++;
-								j++;
-							}
-							aux[i] = '\0';
-							if(aux[0] == '\0')
-								return read;
-							//*ival=ato(aux, i, 16);
-							read++;
-							break;
-			
-				default:
-							c = s[j++];
-							if(!(c != EOF && c != '\n' && *p == c))
-								return read;
-							break;
-			}
-		}
-	va_end(ap); // clean up when done
-	return read;
+    /* Armo el string */
+    while((c = getchar()) != '\n' && c != EOF && i < 50){
+        if(c == '\b'){
+            if(i > 0) i--;
+        }else{
+            s[i++] = c;
+        }
+    }
+    s[i] = '\0';
+    
+    
+    /* Lo analizo */
+    i = 0;
+    char aux [50] = {0};
+    for (p = fmt, i = 0, j = 0; *p; p++){
+        if(*p != '%'){
+            c = s[j++];
+            if( !c || *p != c) return charsRead;
+		}else switch (*++p){
+            case 'd':
+                i = 0;
+                ival = va_arg(ap,int *);
+                while(isdigit(s[j])){
+                    aux[i++] = s[j++];
+				}
+                aux[i] = '\0';
+                if(aux[0] == '\0')  return charsRead;
+                *ival=atoi(aux);
+                charsRead++;
+                break;
+            case 's':
+                i = 0;
+                sval = va_arg(ap,char *);
+                while(s[j] && s[j] != ' '){
+                    aux[i++] = s[j++];
+                }
+                aux[i] = '\0';
+                if(aux[0] == '\0') return charsRead;
+                k = 0;
+                while(aux[k] != '\0'){
+                    sval[k] = aux[k];
+                    k++;
+                }
+                sval[k] = '\0';
+                charsRead++;
+                break;
+            case 'c':
+                cval = va_arg(ap,char*);
+                c = s[j++];
+                if(!c) return charsRead;
+                *cval = c;
+                charsRead++;
+                break;
+            case 'x':
+                i=0;
+                ival = va_arg(ap,int *);
+                while((isdigit(s[j]) || isxdigit(s[j]))){
+                    aux[i++] = s[j++];
+                }
+                aux[i] = '\0';
+                if(aux[0] == '\0') return charsRead;
+                //*ival=ato(aux, i, 16);
+                charsRead++;
+                break;
+            default:
+                c = s[j++];
+                if(c == EOF || c == '\n' || *p != c) return charsRead;
+                break;
+        }
+    }
+    va_end(ap); // clean up when done
+    return charsRead;
 }
-
-*/
