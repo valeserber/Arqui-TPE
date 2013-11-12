ee5f23321dfded91efcd5e38fa44f678c2d098c1
vale <valserber@gmail.com>
2013-11-12 18:30:45 -0300

    jkdf

--- src/asm/libasm.asm
+++ src/asm/libasm.asm
@@ -5,7 +5,6 @@ GLOBAL  _debug
 GLOBAL _opencd
 EXTERN printStatus
 GLOBAL _printError
-EXTERN printNum
 EXTERN printCapacity
 GLOBAL _closecd
 GLOBAL set_cursor
@@ -124,81 +123,6 @@ set_cursor:
     pop     ebp
     ret
 
-_opencd:
-    call    _pollUntilNotBusy
-    ;xor     ax, ax          ;Selects device 0 (master). 10h = device 1 (slave)
-    mov     al,10h
-    mov     dx, 0x1f6       ;Drive/Head (read and write) register address
-    out     dx, al
-    mov     dx, 0x1f1       ;Error (read) and Features (write) register address
-    xor     ax, ax
-    out     dx, al
-    mov     dx, 3f6h        ;Device control register
-    mov     al, 00001010b   ;nIEN = 2nd bit to the right
-    out     dx, al          ;nIEN is now one!
-    mov     dx, 0x1f7       ;Command (write) register address
-    mov     ax, 0xa0        ;0A0h = Packet command
-    out     dx, al          ;Send Packet command
-;After sending the Packet command, the host is to wait 400 nanoseconds
-;before doing anything else.
-;wait:
-    mov     ecx, 0xffff
-waitloop:
-    loopnz  waitloop
-
-;    call    _pollUntilNotBusy
-;    call    _pollUntilDataRequest
-;    mov     dx, 0x1f0       ;Data register
-;    mov     al, 0x1e        ;Prevent/Allow Medium removal Packet command
-;    out     dx, al
-;    xor     ax, ax          ;ax = 0
-;    out     dx, al
-;    out     dx, ax
-;    out     dx, ax
-;    out     dx, ax
-;    out     dx, ax
-;    out     dx, ax
-    call    _pollUntilNotBusy
-    call    _pollUntilDataRequest
-    mov     dx, 0x1f7
-    mov     al, 0xa0
-    out     dx, al
-
-    mov     ecx, 0xffff
-waitloop9:
-    loopnz  waitloop9
-
-    call    _pollUntilNotBusy
-    call    _pollUntilDataRequest
-
-;The command packet has a 12-byte standard format, and the first byte of the
-;command packet contains the actual operation code
-    mov     dx, 0x1f0    ;Data register address
-    mov     ax, 0x1e
-    out     dx, ax
-    xor     ax, ax
-    out     dx, ax
-    out     dx, ax
-    out     dx, ax
-    out     dx, ax
-    out     dx, ax
-    call    _pollUntilNotBusy
-    call    _pollUntilDataRequest
-
-    mov     dx, 0x1f0
-    mov     ax, 0x1b     ;Start/Stop Unit command
-    out     dx, ax
-;The remaining 11 bytes supply parameter info for the command.
-    xor     ax, ax
-    out     dx, ax
-    mov     ax, 2       ;LoEj bit in 1, Start bit in 0: Eject disc if possible
-    out     dx, ax
-    xor     ax, ax
-    out     dx, ax
-    out     dx, ax
-    out     dx, ax
-    call    _pollUntilNotBusy
-    ret
 
 _closecd:
     call    _pollUntilNotBusy
